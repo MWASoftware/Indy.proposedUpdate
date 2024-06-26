97e509c3b1100619476b30ea724ac9e64b139454
Vilis Sviklis <vilis.sviklis@tieto.com>
2024-06-26 23:40:11 +0300

    Revert "Change to priority order of ssl libraries and fix to compile under old Delphi versions"
    This reverts commit e50fdd0244ab9a7b0b2a6d77d2abe520bc4b3a1f.

--- Lib/Protocols/IdSSLOpenSSL.pas
+++ Lib/Protocols/IdSSLOpenSSL.pas
@@ -286,7 +286,7 @@ uses
 
 type
   TIdSSLVersion = (sslUnknown,sslvSSLv2, sslvSSLv23, sslvSSLv3, sslvTLSv1,sslvTLSv1_1,
-    sslvTLSv1_2, sslvTLSv1_3); {May need to update constants below if adding to this set}
+    sslvTLSv1_2, sslvTLSv1_3);
   TIdSSLVersions = set of TIdSSLVersion;
   TIdSSLMode = (sslmUnassigned, sslmClient, sslmServer, sslmBoth);
   TIdSSLVerifyMode = (sslvrfPeer, sslvrfFailIfNoPeerCert, sslvrfClientOnce);
@@ -297,7 +297,6 @@ type
 const
   DEF_SSLVERSION = sslvTLSv1_2;
   DEF_SSLVERSIONS = [sslvTLSv1_2,sslvTLSv1_3];
-  MAX_SSLVERSION = sslvTLSv1_3;
   P12_FILETYPE = 3;
   MAX_SSL_PASSWORD_LENGTH = 128;
 
@@ -3578,7 +3577,7 @@ begin
   begin
     if SSLVersions <> [] then
     begin
-      for v := sslvSSLv3 to MAX_SSLVERSION do
+      for v := sslvSSLv3 to high(TIdSSLVersions) do
       begin
         if v in SSLVersions then
         begin
@@ -3586,7 +3585,7 @@ begin
           break;
         end;
      end;
-      for v := MAX_SSLVERSION downto sslvSSLv3 do
+      for v := high(TIdSSLVersions) downto sslvSSLv3 do
       begin
         if v in SSLVersions then
         begin
